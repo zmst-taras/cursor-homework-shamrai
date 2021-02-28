907b21256abe7a64897aab98973e404062d6b86d
zmst-taras <rapeme01@gmail.com>
2021-02-28 17:18:56 +0200

    Ex3

---
+++ hw3/hw3.js
@@ -0,0 +1,25 @@
+// Ex# 1
+
+// function getMaxDigit (number) {
+//   if (n <= 0){
+//     return 0
+//   }else {
+//       return Math.max (n %10, getMaxDigit(Math.floor(n / 10)));
+
+//     }
+//   }
+// console.log(`${getMaxDigit}`);
+
+
+ let getMaxDigit = (n) => {
+   if (Number(n) <= 0){
+     return (Math.max())
+   }else{
+   n === Number.NaN
+  
+   }
+    console.log("It`s not number");
+ }
+ 
+ console.log(getMaxDigit(Math.max(13161351353)));
+
