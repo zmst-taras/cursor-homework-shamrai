4a8aed7879504bb125fc4eba4852a7914577ad54
zmst-taras <rapeme01@gmail.com>
2021-03-02 14:42:42 +0200

    changes 02.03.21

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -2,66 +2,45 @@
  
 // Ex# 1 - 2 
 
-let numberN;
+let numberN = null;
 do{
-  numberN = +(prompt("Введіть число N "));
-    console.log ("number N:" ,numberN);
-
-     if (numberN % 1 === 0) {
-
-      numberN     
-
-      } else{
-      
-        alert("Число має бути цілим. Будь ласка повторіть спробу")
-    }
+  numberN = +(prompt("Введіть число N "));  
     
 }
-while( Number.isNaN(numberN));
+
+while( !Number.isInteger(numberN));
+console.log ("number N:" ,(numberN));
 
 //  Ex# 3 - 4 
 
-let numberM
+let numberM = null;
 do{
   numberM = +(prompt("Введіть число M "));
-    console.log ("number M: ",numberM);
 
-      if (numberM % 1 === 0) {
-
-         numberM    
-
-      } else{
-    
-      alert("Число має бути цілим. Будь ласка повторіть спробу")
-   }
-   
 }
- while( Number.isNaN(numberM));
+
+ while (!Number.isInteger(numberM));
+ console.log ("number M: ",(numberM));
 
 //  Ex# 5
 
-  let skipEvenNumber  = confirm(" чи потрібно пропускати парні числа ?")
-skipEvenNumber? "парне" : "не парне ";
-    console.log ("пропускати парне:" ,skipEvenNumber,"число");
+  const skipEvenNumber  = confirm(" чи потрібно пропускати парні числа ?")
+    console.log ("пропускати:" ,skipEvenNumber,"число");
   
 // Ex# 6 - 7
 let sumN_M = 0
-if (skipEvenNumber === true ){
 
   for( let y = numberN; y <= numberM; y++){
 
-      if ( y % 2 === 0){
+      if (skipEvenNumber && y % 2 === 0){
+
           continue;
-      } sumN_M += y;
-    }
-  } else {
-      for( let y = numberN; y <= numberM; y++){
+        }
         sumN_M += y;
-      }
-  }  
+  }
+    
   
-console.log ("sum at number N do number M:" ,sumN_M,);
+console.log ("sum at number N to number M:" ,sumN_M,);
 document.writeln("sum numbers at number N do number M: " ,sumN_M);
 
 
-  
--- hw3/hw3.js
+++ hw3/hw3.js
@@ -11,15 +11,37 @@
 // console.log(`${getMaxDigit}`);
 
 
- let getMaxDigit = (n) => {
-   if (Number(n) <= 0){
-     return (Math.max())
-   }else{
-   n === Number.NaN
-  
-   }
-    console.log("It`s not number");
- }
+//  let getMaxDigit = function(n) {
+//    if (n <= 0){
+//      n === Number.isNaN
+//      return (Math.max(n));
+//    }else{
+//    n !== NaN
+//   alert("It`s not number");
+//    }
+    
+//  }
  
- console.log(getMaxDigit(Math.max(13161351353)));
+//  console.log(getMaxDigit(Math.max(13161351353)));
 
+
+//   `);
+  function getMaxDigit (number){
+    const numberToString = String(number).split('');
+    const maxNumber = Math.max(...numberToString);
+  return maxNumber;
+  }
+  console.log("функція №1:" , getMaxDigit(1321564648132));
+  document.writeln(`Максимальне число: ${getMaxDigit(125489411)} <br>`);
+
+
+  // Ex#2
+
+  
+ function degreeOf (x,y){
+   for (let i = 0; i < y; i++) {
+     const degree = x;
+     x += degree;
+   }
+   return x;
+ }
