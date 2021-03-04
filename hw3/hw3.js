052416cb91f6d4b098f13f90a96cf784aae1558f
zmst-taras <rapeme01@gmail.com>
2021-03-04 14:50:23 +0200

    Hw3

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -1,47 +1,82 @@
-// Ex# 1
+//   І завдання, Знайти максимальне число;
 
-// function getMaxDigit (number) {
-//   if (n <= 0){
-//     return 0
-//   }else {
-//       return Math.max (n %10, getMaxDigit(Math.floor(n / 10)));
-
-//     }
-//   }
-// console.log(`${getMaxDigit}`);
-
-
-//  let getMaxDigit = function(n) {
-//    if (n <= 0){
-//      n === Number.isNaN
-//      return (Math.max(n));
-//    }else{
-//    n !== NaN
-//   alert("It`s not number");
-//    }
-    
-//  }
- 
-//  console.log(getMaxDigit(Math.max(13161351353)));
-
-
-//   `);
   function getMaxDigit (number){
     const numberToString = String(number).split('');
     const maxNumber = Math.max(...numberToString);
   return maxNumber;
   }
   console.log("функція №1:" , getMaxDigit(1321564648132));
-  document.writeln(`Максимальне число: ${getMaxDigit(125489411)} <br>`);
+  document.writeln(`Максимальне число: ${getMaxDigit(123456789)} <br>`);
 
-
-  // Ex#2
-
-  
- function degreeOf (x,y){
+  // ІІ завдання. Функція - яка визначає ступінь числа
+ 
+ function degreeOfNumber (x,y){
    for (let i = 0; i < y; i++) {
      const degree = x;
      x += degree;
    }
    return x;
  }
+ console.log ("ступінь числа:" ,degreeOfNumber(2,5));
+ document.writeln(`ступінь числа: ${degreeOfNumber(2,5)} <br>`);
+
+  // ІІІ завдання, функція щ форматує ім'я / Зроблено  через рефакторінг
+
+function nameFormat (name){
+  const name2 = String(name).toLowerCase();
+  const upperName = name2.charAt(0).toUpperCase() +  name2.slice(1);
+
+  return upperName;
+} 
+
+function nameFormatRefactoring2 (name){
+  const name2 = String(name).toLowerCase();
+  return name2.charAt(0).toUpperCase() +  name2.slice(1);
+} 
+
+function nameFormatRefactoring3 (name){
+  return name.charAt(0).toUpperCase() +  name.slice(1).toLowerCase();
+} 
+console.log("nameFormat : function = ", nameFormat("fuNctIon"));
+console.log("nameFormatRefactoring : function = ", nameFormatRefactoring2("fuNction"));
+console.log("nameFormatRefactoring2 : function = ", nameFormatRefactoring3("фуНКЦіЯ"));
+
+// ІV завдання ,  функція що залишає суму, після оплат податків
+
+function getTaxes (sum){
+  const  tax = 1 - ((18 + 1.5) /100);
+  const division = (sum  * tax).toFixed(2) ;
+
+  return division;
+}
+console.log("сума після податків:" ,getTaxes(1000));
+document.writeln(`сума після податків: ${getTaxes(1000)} <br>`)
+
+
+// V завдання ,  функція що повертає рандомне число
+
+function randomNumber(n,m){
+  const getRandom = Math.round(Math.random() *  (m - n) + n);
+
+  return getRandom;
+}
+console.log("рандомне число:" ,randomNumber(-100,100))
+document.writeln(`рандомне число: ${randomNumber(-100,100)}`);
+
+// VІ завдання ,  "повторение - мать учение"
+
+ function searchLetters (letters, word){
+   let counter = 0 ;
+   for ( let i = 0; i < word.length; i++ ){
+     if (word [i] === letters) 
+     counter++;
+   }
+   return counter;
+ }
+ console.log(`повторення літери "а" в слові "парадігма"   ${searchLetters('а',  'парадігма')}`);
+
+
+
+
+
+
