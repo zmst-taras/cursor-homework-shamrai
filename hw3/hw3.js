d8e534212d4c14e202ae8b256f432b5dce6f124a
zmst-taras <rapeme01@gmail.com>
2021-03-07 16:57:16 +0200

    change after fixed 07.03.21

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -10,17 +10,17 @@
 
   // ІІ завдання. Функція - яка визначає ступінь числа
  
- function degreeOfNumber (x,y){
-   for (let i = 0; i < y; i++) {
-     const degree = x;
-     x += degree;
+ function degreeOfNumber (numberX,numberY){
+   let result = 1;
+   for (let i = 0; i < numberY; i++) {
+     result *= numberX
    }
-   return x;
+   return result;
  }
  console.log ("ступінь числа:" ,degreeOfNumber(2,5));
  document.writeln(`Функція №2: ступінь числа: ${degreeOfNumber(2,5)} <br>`);
 
-  // ІІІ завдання, функція щ форматує ім'я / Зроблено  через рефакторінг
+  // ІІІ завдання, функція що форматує ім'я / Зроблено  через рефакторінг
 
 function nameFormat (name){
   const name2 = String(name).toLowerCase();
@@ -30,17 +30,13 @@ function nameFormat (name){
 } 
 
 function nameFormatRefactoring2 (name){
-  const name2 = String(name).toLowerCase();
-  return name2.charAt(0).toUpperCase() +  name2.slice(1);
+  const firstLetter = String(name).toLowerCase();
+  return firstLetter.charAt(0).toUpperCase() +  firstLetter.slice(1);
 } 
 
-function nameFormatRefactoring3 (name){
-  return name.charAt(0).toUpperCase() +  name.slice(1).toLowerCase();
-} 
 console.log("nameFormat : function = ", nameFormat("fuNctIon"));
 document.writeln(` Функція №3: ім'я з Великої літери: ${nameFormat("фУНкЦіЯ", "аНАсТаСіЯ")} <br>`);
-console.log("nameFormatRefactoring : function = ", nameFormatRefactoring2("fuNction"));
-console.log("nameFormatRefactoring2 : function = ", nameFormatRefactoring3("фуНКЦіЯ"));
+console.log("nameFormatRefactoring2 : function = ", nameFormatRefactoring2("фуНКЦіЯ"));
 
 // ІV завдання ,  функція що залишає суму, після оплат податків
 
@@ -56,8 +52,8 @@ document.writeln(`Функція №4: сума після податків: ${g
 
 // V завдання ,  функція що повертає рандомне число
 
-function randomNumber(n,m){
-  const getRandom = Math.round(Math.random() *  (m - n) + n);
+function randomNumber(min,max){
+  const getRandom = Math.round(Math.random() *  (max - min) + min);
 
   return getRandom;
 }
@@ -66,7 +62,7 @@ document.writeln(`Функція №5: рандомне число: ${randomNumb
 
 // VІ завдання ,  "повторение - мать учение"
 
- function searchLetters (letters, word){
+ function countLetters (letters, word){
    const lettersLower = letters.toLowerCase();
    const wordLower = word.toLowerCase();
 
@@ -77,10 +73,10 @@ document.writeln(`Функція №5: рандомне число: ${randomNumb
    }
    return `повторення літери "${letters}" в реченні " ${word} " в кількості ${counter} рази`;
  }
- const searchLetters2 = searchLetters( 'р', 'Круїз Тома Круза кращий');
+ const countLetters2 = countLetters( 'р', 'Круїз Тома Круза кращий');
 
- console.log(searchLetters("р",  "Круїз Тома Круза кращий"));
- document.writeln(`Функція №6:  ${searchLetters2}`);
+ console.log(countLetters("р",  "Круїз Тома Круза кращий"));
+ document.writeln(`Функція №6:  ${countLetters2}`);
 
 
 
