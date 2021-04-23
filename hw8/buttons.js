82c77ece413eca0e52fa267296608f218a413043
Taras <rapeme01@gmail.com>
2021-04-23 10:44:06 +0300

    file buttons

---
+++ hw8/buttons.js
@@ -0,0 +1,10 @@
+
+    // buttons onclick
+    //........................................................................
+     function text1(){
+      document.getElementsById("hi").append("Thaks for visit") 
+    };
+     function text2(){
+      document.getElementsById("hey").append("Сome again")
+    };
+    //.........................................................................
