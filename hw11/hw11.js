ac1d0d0936de47dae6a775283a78ceeaa5637b02
Taras <rapeme01@gmail.com>
2021-05-18 13:29:02 +0300

    added my drum kit with special sounds [2.0]

--- hw11/hw11.js
+++ hw11/hw11.js
@@ -1,4 +1,5 @@
 window.addEventListener('keydown', function (e) {
+    console.log(e);
 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
